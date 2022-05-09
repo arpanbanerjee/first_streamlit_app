4581020346bdde28dbbe49631b57463bb839c97f
Arpan Banerjee <severus.banerjee@gmail.com>
2022-05-09 10:41:30 +0530

    Update streamlit_app.py
    Third Commit: Adding Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit 
 streamlit.title("My Mom\'s New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach and Rocket Smoothie")
+strealit.text("Hard Boiled Free Range Egg")
