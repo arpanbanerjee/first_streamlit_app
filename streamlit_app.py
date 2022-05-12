baa34305d1f9ed81c39f2c0fd4ce882b47d10fcc
Arpan Banerjee <severus.banerjee@gmail.com>
2022-05-12 14:40:23 +0530

    Update streamlit_app.py
    Trying to add the actual fruit name and changing button name and closing connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ except URLError as e:
 #take the json version of the response and normalize it
 
 #output it at the screen as a table
-streamlit.header("The fruit load list contains: ")
+streamlit.header("View our Fruit List - Add Your Favourites!")
 
 #Snowflake related functions: 
 def get_fruit_load_list():
@@ -51,21 +51,23 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 #Add a button to load the fruit:
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_row = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_row)
 
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    my_cur.execute("insert into fruit_load_list values('"new_fruit"')")
     return "Thanks for adding "+new_fruit
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
 
 streamlit.stop()
